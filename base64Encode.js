bcd2703b5fa5fa8747ac7bb4833dcf70b88ab60f
daylim95 <daylim95@gmail.com>
2022-03-13 20:41:22 +0900

    base64 encode
    base64 encode done in console

--- base64Encode.js
+++ base64Encode.js
@@ -1,64 +1,67 @@
 $("#base64Encode").on("click", function(){
 
-    var inputStr = $("#inputStr").val();
-    var asciiArr = new Array();
-    for(var i=0;i<inputStr.length;i++){
-        asciiArr.push(inputStr.charCodeAt(i));
-    }
-
-    var binaryStr = "";
+    var inputStr = $("#inputStr").val()
+    var binaryStr = ""
     for(var j=0;j<asciiArr.length;j++){
-        var bin =  asciiArr[j].toString(2);
-        var zeros = 8-bin.length;
+        var bin =  inputStr.charCodeAt(i).toString(2)
+        var zeros = 8-bin.length
         for(var k=0;k<zeros;k++){
-            bin = "0" + bin;
+            bin = "0" + bin
         }
-        binaryStr += bin;
+        binaryStr += bin
     }
     
-    var binaryLength = binaryStr.length;
+    var binaryLength = binaryStr.length
     if(binaryLength%24!=0){
         for(var l=0;l<(24-binaryLength%24);l++){
-            binaryStr += "0";
+            binaryStr += "0"
         }
     }
 
     var binaryBlock = binaryStr.length/6;
-    var binaryArr = new Array();
-    for(var i=0;i<binaryBlock;i++){
-        binaryArr.push(binaryStr.substr(i*6,6));
+    var encodedString = ""
+    for(var m=0;m<binaryBlock;m++){
+        encodedString += Base64Index(BinToDec(binaryStr.substr(m*6,6)))
     }
-    console.log(binaryArr)
-    
 
+    console.log(encodedString)
 })
 
 
-// base64 인덱스를 ascii 로 변환
+// base64 인덱스를 ascii 인덱스로 변환
 function Base64Index(integer){
     
     if(integer<26){
         // 'A~Z'
-        integer += 65;
+        integer += 65
     }else if(integer<52){
         // 'a~z'
-        integer += 71;
+        integer += 71
     }else if(integer<62){
         // '0~9'
-        integer -= 4;
+        integer -= 4
     }else if(integer==62){
         // '+'
-        integer = 43;
+        integer = 43
     }else if(integer==63){
         // '/'
-        integer = 47;
+        integer = 47
     }
 
-    return String.fromCharCode(integer);
+    return String.fromCharCode(integer)
 }
 
 function BinToDec(binary){
-    for(var i=0;i<binary.length;i++){
-        var bin = binary%10;
+    var idx = 1
+    var decimal = 0
+
+    while(idx<=32){
+
+        var val = binary%10
+        decimal += val * idx
+        
+        binary = parseInt(binary/10)
+        idx *= 2
     }
+    return decimal
 }
